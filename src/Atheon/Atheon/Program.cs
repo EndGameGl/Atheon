35ac16f46d61aa21ecec6764bee9921f19e2a859
MeGl <neloangelo3105@gmail.com>
2023-01-08 13:39:26 +0300

    Adding in default asp.net bootstrap code

--- src/Atheon/Atheon/Program.cs
+++ src/Atheon/Atheon/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Serilog;
 
-Console.WriteLine("Hello, World!");
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    ConfigureServices(builder);
+
+    var application = builder.Build();
+
+    await application.RunAsync();
+}
+catch (Exception exception)
+{
+    Log.Logger.Error(exception, "Failed to start app");
+}
+
+void ConfigureServices(WebApplicationBuilder applicationBuilder)
+{
+    applicationBuilder.Host.UseSerilog((context, services, configuration) =>
+    {
+        configuration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services.GetService<IServiceProvider>())
+            .Enrich.FromLogContext()
+            .WriteTo.Console();
+    });
+}
